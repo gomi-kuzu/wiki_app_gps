5698ee6c62c2d9806b8a55b43fb97f13860b1159
inoma <syookiti@gmail.com>
2020-09-02 20:31:56 +0900

    refix

--- sw.js
+++ sw.js
@@ -1,12 +1,25 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+const STATIC_DATA = [
+  'index.html',
+  '/css/materialize.css',
+  '/css/materialize.min.css',
+  '/js/materialize.js',
+  '/js/materialize.min.js',
+];
 
-self.addEventListener('fetch', function(e) {
-  // ファイルのキャッシュ
-  workbox.precaching.precacheAndRoute([
-    {
-      url: '/index.html',
-      revision: '12345'
-    },
-  ])
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('cache_v1').then(function(cache) {
+     return cache.addAll(STATIC_DATA);
+   })
+ );
+});
 
-  })
+self.addEventListener('fetch', function(event) {
+ console.log(event.request.url);
+
+ event.respondWith(
+   caches.match(event.request).then(function(response) {
+     return response || fetch(event.request);
+   })
+ );
+});
