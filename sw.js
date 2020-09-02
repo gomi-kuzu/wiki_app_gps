ab0fcc153b39201cde47b0c629c287bd40cdef02
inoma <syookiti@gmail.com>
2020-09-02 20:21:31 +0900

    make cach

--- sw.js
+++ sw.js
@@ -1,3 +1,12 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+
 self.addEventListener('fetch', function(e) {
-    // ここは空でもOK
+  // ファイルのキャッシュ
+  workbox.precaching.precacheAndRoute([
+    {
+      url: '/index.html',
+      revision: '12345'
+    },
+  ])
+
   })
